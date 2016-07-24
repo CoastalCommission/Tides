daeeaf83a5959b640b114d87c0133cb6eee78746
Zachariah Moreno <xachmoreno@gmail.com>
2016-07-23 18:06:53 -0700

    added icon & category data to hero

--- frontend/app/services/hero/data/hero.data.ts
+++ frontend/app/services/hero/data/hero.data.ts
@@ -1,14 +1,65 @@
-import { Hero } from '../../components/hero/hero';
+import { Hero } from '../type/hero.type';
 
 export var HEROES: Hero[] = [
-    { 'id': 11, 'name': 'Mr. Nice' },
-    { 'id': 12, 'name': 'Narco' },
-    { 'id': 13, 'name': 'Bombasto' },
-    { 'id': 14, 'name': 'Celeritas' },
-    { 'id': 15, 'name': 'Magneta' },
-    { 'id': 16, 'name': 'RubberMan' },
-    { 'id': 17, 'name': 'Dynama' },
-    { 'id': 18, 'name': 'Dr IQ' },
-    { 'id': 19, 'name': 'Magma' },
-    { 'id': 20, 'name': 'Tornado' }
+    {
+        'id': 1,
+        'name': 'Access Locations',
+        'icon': 'beach_access',
+        'category': ''
+    }, {
+        'id': 2,
+        'name': 'Cleanup Day Locations',
+        'icon': 'public',
+        'category': ''
+    }, {
+        'id': 3,
+        'name': 'CZB',
+        'icon': 'explore',
+        'category': ''
+    }, {
+        'id': 4,
+        'name': 'LCPs',
+        'icon': 'extension',
+        'category': ''
+    }, {
+        'id': 5,
+        'name': 'Press Pieces',
+        'icon': 'format_quote',
+        'category': ''
+    }, {
+        'id': 6,
+        'name': 'Social Media Posts',
+        'icon': 'share',
+        'category': ''
+    }, {
+        'id': 7,
+        'name': 'Agendas',
+        'icon': 'gavel',
+        'category': ''
+    }, {
+        'id': 8,
+        'name': 'Reports',
+        'icon': 'description',
+        'category': ''
+    }, {
+        'id': 9,
+        'name': 'Results',
+        'icon': 'assignment_turned_in',
+        'category': ''
+    }, {
+        'id': 10,
+        'name': 'Time',
+        'icon': 'access_time',
+        'category': ''
+    }, {
+        'id': 11,
+        'name': 'Accounts',
+        'icon': 'account_balance',
+        'category': ''
+    }, {
+        'id': 12,
+        'name': 'Contracts',
+        'icon': 'folder_shared',
+        'category': ''
+    }
 ];
