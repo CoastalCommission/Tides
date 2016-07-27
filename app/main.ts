c45900782fe1525b2c0ab6a3ffdc6a255e3d9c1b
Zachariah Moreno <xachmoreno@gmail.com>
2016-07-26 23:34:58 -0700

    switched to polymer

--- app/main.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrap }         from 'angular2/platform/browser';
-import { HTTP_PROVIDERS }    from 'angular2/http';
-import { TidesAppComponent } from './components/tides-app/tides-app.component';
-
-bootstrap(TidesAppComponent, [ HTTP_PROVIDERS ]);
